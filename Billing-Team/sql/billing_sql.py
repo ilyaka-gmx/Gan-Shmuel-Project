c157c530bbd32d92a16d03f5fac5f5cbd239e1cc
iddchn <idan@nexil.co.il>
2024-11-14 16:44:10 +0200

    Adding test for the query and I test the query on another page using import

--- Billing-Team/sql/billing_sql.py
+++ Billing-Team/sql/billing_sql.py
@@ -5,7 +5,7 @@ db_config = {
     'host': os.environ.get("DB_HOST", "host.docker.internal"),
     'port': int(os.environ.get("DB_PORT", 3306)),
     'user': os.environ.get("DB_USER", "root"),
-    'password': os.environ.get("MYSQL_ROOT_PASSWORD", "123456"),
+    'password': os.environ.get("MYSQL_ROOT_PASSWORD", ""),
     'database': os.environ.get("MYSQL_DATABASE", "billdb"),
 }
 
@@ -14,16 +14,12 @@ Table.connect(config_dict=db_config)
 Providers = get_table('Provider')
 Rates = get_table('Rates')
 Trucks = get_table('Trucks')
-# def init_db_tables():
-#     with mysql.connector.connect(**db_config) as conn:
-#         with conn.cursor() as cursor:
-#             cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(db_config['database']))
 
+#test function needs to remove after first API will work with the BD
 def test_function():
-    new_provider = Providers.create(name='test')
-    print(new_provider)
+    new_provider = Providers.create(name='dany')
+    provider = Providers.find(id=10001)
+    print(provider.name)
 
 if __name__ == '__main__':
-    # Initialize database
-    # init_db()
     test_function()
